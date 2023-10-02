2d50ded20ce885d526dbaa61d08b83836bcc30a0
bo <61472065+bwd202@users.noreply.github.com>
2023-10-02 08:21:03 -0400

    add #reminder-modal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-// alert('helo')
-
 import './style.css'
+
+class Reminder {
+
+    constructor(title, notes, dueDate, priority) {
+        this.title = title
+        this.notes = notes
+        this.dueDate = dueDate
+        this.priority = priority
+    }
+}
+
+// console.log(new Reminder('helo',new Date(), 3))
+
+let newReminderBtn = document.querySelector('#new-reminder')
+
+// newReminderBtn.addEventListener('click', ()=> console.log('helo'))
+
+let reminderModal = document.querySelector('#reminder-modal')
+
+reminderModal.hidden = false
