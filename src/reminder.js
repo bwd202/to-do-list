de746287488036db300c7199455f01d0bdcf281a
bo <61472065+bwd202@users.noreply.github.com>
2023-10-06 16:50:41 -0400

    write reminder module

---
+++ src/reminder.js
@@ -0,0 +1,13 @@
+
+
+class Reminder {
+
+    constructor({title="title", notes="notes", dueDate="dueDate", dueTime="dueTime", priority="medium"} = {}) {
+        this.title = title
+        this.notes = notes
+        this.dueDate = dueDate
+        this.dueTime = dueTime
+        this.priority = priority
+    }
+
+}
--- src/reminderHTML.js
+++
@@ -1,5 +0,0 @@
-export {createReminderHTML}
-
-function createReminderHTML() {
-    
-}
