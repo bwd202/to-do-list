cafc635f961f45dd40adcc54f6fd4d25c53125f3
bo <61472065+bwd202@users.noreply.github.com>
2023-10-21 14:10:14 -0400

    add completed param to Reminder class

--- src/reminder.js
+++ src/reminder.js
@@ -3,13 +3,14 @@ import {countReminders} from './list'
 
 class Reminder {
 
-    constructor({title="title", notes="notes", dueDate="dueDate", dueTime="dueTime", priority="medium", list="default"} = {}) {
+    constructor({title="title", notes="notes", dueDate="dueDate", dueTime="dueTime", priority="medium", list="default", completed=false} = {}) {
         this.title = title
         this.notes = notes
         this.dueDate = dueDate
         this.dueTime = dueTime
         this.priority = priority
         this.list = list
+        this.completed = completed
     }
 
 }
