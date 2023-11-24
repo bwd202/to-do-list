3a829194fff0bb2bcd85115181d5dea530aa3091
bo <61472065+bwd202@users.noreply.github.com>
2023-11-24 11:54:04 -0500

    refactor markComplete fn

--- src/completed.js
+++ src/completed.js
@@ -1,53 +1,61 @@
 // deals with reminder completion feature
-import { reminders } from "./storage"
-export {crossOutHtml}
+import { reminders } from "./storage";
+export { crossOutHtml };
 
-let completed = []
+let completed = [];
 
 function crossOutHtml(e) {
 
-    if(e.target.type === 'checkbox') {
+    if (e.target.type === "checkbox") {
 
         // console.log('checkbox clicked')
 
-        e.target.nextElementSibling.classList.toggle('completed')
+        e.target.nextElementSibling.classList.toggle("completed");
 
-        let reminder = e.target.nextElementSibling.children[0].innerHTML
+        let reminder = e.target.nextElementSibling.children[0].innerHTML;
 
-        getCompleted(reminder)
-    } 
+        markComplete(reminder);
+    }
+}
+
+function markComplete(item) {
+    // sets reminderCompleted prop to true
+
+    for(let i = 0; i < reminders.length; i++) {
+
+        if(!reminders[i].reminderCompleted) {
+
+            if(reminders[i].reminderTitle === item) {
+
+                reminders[i].reminderCompleted = true
+            } 
+
+        } else if(reminders[i].reminderCompleted) {
+
+            if(reminders[i].reminderTitle === item) {
+
+                reminders[i].reminderCompleted = false
+            }
+        }
+    }
+
+    console.log(reminders);
 }
 
 function getCompleted(reminder) {
     // changes reminderCompleted prop
 
-    for(let i = 0; i < reminders.length; i++) {
-
-        if(reminder === reminders[i].reminderTitle) {
-
-            if(!completed.includes(reminder)) {
-
-                completed.push(reminders[i])
+    for (let i = 0; i < reminders.length; i++) {
+        if (reminder === reminders[i].reminderTitle) {
+            if (!completed.includes(reminder)) {
+                completed.push(reminders[i]);
             }
         }
     }
-    
-    markComplete()
 
-
-    console.log(completed)
-}
-
-function markComplete() {
-    // sets reminderCompleted prop to true
-
-    for(let i = 0; i < completed.length; i++) {
-
-        completed[i].reminderComplete = true
-    }
+    console.log(completed);
 }
 
 function showCompleted() {
     // shows completed reminders in modal
-
 }
