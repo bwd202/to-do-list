a4a35fe60cb32ff5f770c341bf2524f11f542566
bo <61472065+bwd202@users.noreply.github.com>
2023-11-30 14:23:49 -0500

    add chevron css class

--- src/banner.js
+++ src/banner.js
@@ -5,5 +5,14 @@ function showDropDown(e) {
     if(e.target.classList.contains('banner')) {
         
         e.target.lastElementChild.classList.toggle('visible')
+
+        reverseChevron()
     }
+}
+
+function reverseChevron() {
+
+    let container = document.querySelectorAll('.chevron')
+
+    container.innerHTML = '2b9d'
 }
