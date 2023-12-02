708f3286f27e85cd256864c4fcd91ce039146781
bo <61472065+bwd202@users.noreply.github.com>
2023-12-02 12:01:55 -0500

    add icon1 var in makeList

--- main.js
+++ main.js
@@ -4947,7 +4947,11 @@ function makeHtmlList(obj) {    //creates html list banner from obj
 
     listBanner.classList.add('banner','button','border')
 
-    let icon = new _icon__WEBPACK_IMPORTED_MODULE_1__.listIcon().make()
+    let icon = new _icon__WEBPACK_IMPORTED_MODULE_1__.listIcon()
+
+    icon.color = obj.listColor
+
+    let icon1 = icon.make()
 
     // let iconColor = CssFilterConverter.hexToFilter(obj.listColor).color  //color is a prop of the obj returned by cssFilterConverter
 
@@ -4977,7 +4981,7 @@ function makeHtmlList(obj) {    //creates html list banner from obj
 
     dropDown.setAttribute('hidden','')
 
-    listBanner.append(icon, p, count, closeBtn, dropDown)
+    listBanner.append(icon1, p, count, closeBtn, dropDown)
 
     wrapper.append(listBanner)
 
