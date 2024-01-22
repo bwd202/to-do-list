98b9babfba6e91589db81e33af1556eb989ba262
bo <61472065+bwd202@users.noreply.github.com>
2024-01-22 12:48:12 -0500

    add html modal for editting reminders

--- src/edit.js
+++ src/edit.js
@@ -1,30 +1,9 @@
 export {editReminder}
 
-function makeReminderModal() {
-
-    let modal = document.createElement('div')
-
-    modal.classList.add('modal')
-
-    // modal.setAttribute('hidden', true)
-
-    let closeBtn = document.createElement('span')
-
-    closeBtn.classList.add('button', 'modal-close-btn')
-
-    let content = document.createElement('div')
-
-    content.classList.add('modal-content')
-
-    modal.append(closeBtn,content)
-
-    return modal
-}
-
 function editReminder(e) {
 
     if(e.target.classList.contains('reminder-short')) {
 
-        document.querySelector('body').append(makeReminderModal())
+        document.querySelector('#edit-reminder').classList.toggle('visible')
     }
 }
